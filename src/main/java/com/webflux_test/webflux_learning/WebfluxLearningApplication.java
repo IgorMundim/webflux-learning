a3cce8d699d89a0a16c634ad808d2549f1ec4bbe
Igor Mundim <mundim@Igors-MacBook-Pro.local>
2024-06-11 12:54:10 -0300

    core: learning

---
+++ src/main/java/com/webflux_test/webflux_learning/WebfluxLearningApplication.java
@@ -0,0 +1,13 @@
+package com.webflux_test.webflux_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webflux_test/webflux_learning/WebfluxLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux_test.webflux_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
